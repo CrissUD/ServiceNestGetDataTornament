109ba80d66f767c53089b488b570a2bf406830f6
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 12:16:33 -0500

    Se crean servicios para obtencion de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { GetTeamDataController } from './controllers/get-team-data/get-team-data.controller';
 import { GetTournamentDataController } from './controllers/get-tournament-data/get-tournament-data.controller';
 import { GetMatchDataController } from './controllers/get-match-data/get-match-data.controller';
+import { TournamentService } from './services/tournament/tournament.service';
+import { TeamService } from './services/team/team.service';
+import { MatchService } from './services/match/match.service';
 
 @Module({
   imports: [],
   controllers: [AppController, GetTeamDataController, GetTournamentDataController, GetMatchDataController],
-  providers: [AppService],
+  providers: [AppService, TournamentService, TeamService, MatchService],
 })
 export class AppModule {}
