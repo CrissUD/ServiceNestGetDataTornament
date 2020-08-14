187633898c0e02cfa4fee02094f3f1b23d9d2d23
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-08-14 13:19:07 -0500

    generacion de métodos para obtencion de infromacion

--- src/controllers/get-tournament-data/get-tournament-data.controller.ts
+++ src/controllers/get-tournament-data/get-tournament-data.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Get } from '@nestjs/common';
+import { TournamentService } from 'src/services/tournament/tournament.service';
 
-@Controller('get-tournament-data')
-export class GetTournamentDataController {}
+@Controller('api/tournament')
+export class GetTournamentDataController {
+    constructor (private tournamentService : TournamentService) {}
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    @Get("/:idTournament")
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    getTeams(@Param('idTournament') idTournament) {
+        return this.tournamentService.getTournament(idTournament);
+    }
+}
--- src/services/tournament/tournament.service.ts
+++ src/services/tournament/tournament.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { FirebaseService } from '../firebase/firebase.service';
 
 @Injectable()
-export class TournamentService {}
+export class TournamentService {
+    constructor(private firebaseService: FirebaseService) { }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    async getTournament (idTournament) {
+        return await this.firebaseService.getElement(`/json/${idTournament}/`);
+    }
+}
